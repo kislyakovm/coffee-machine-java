c0ebe0d0079b3fc3f3e405ebd39267e34e0b6a1f
kislyakovm <maksim@druble.ru>
2022-12-01 01:30:55 +0300

    stage 3/6. Add count of cups calculator, add new class StandardCoffee, decompose code

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,31 +4,58 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        cupsCalculating();
+
+        int water = inputIngredientQuantity("water", "ml");
+        int milk = inputIngredientQuantity("milk", "ml");
+        int coffeeBeans = inputIngredientQuantity("coffee beans", "grams");
+        int cups = inputCupsQuantity();
+
+        cupsCalculating(water, milk, coffeeBeans, cups);
     }
 
-    public static void cupsCalculating() {
+    public static int inputIngredientQuantity(String ingredient, String metricUnit) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Write how many cups of coffee you will need: ");
-        int numOfCups = scanner.nextInt();
-
-        System.out.printf("For %d cups of coffee you will need:\n", numOfCups);
-        System.out.printf("%d ml of water\n", numOfCups * 200);
-        System.out.printf("%d ml of milk\n", numOfCups * 50);
-        System.out.printf("%d g of coffee beans\n", numOfCups * 15);
+        System.out.printf("Write how many %s of %s the coffee machine has:\n", metricUnit, ingredient);
+        return scanner.nextInt();
     }
 
-    public void printActions() {
-        String standardMessages = """
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!""";
+    public static int inputCupsQuantity() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need: ");
+        return scanner.nextInt();
+    }
 
-        System.out.println(standardMessages);
+    public static void cupsCalculating(int water, int milk, int coffeeBeans, int cups) {
+
+        StandardCoffee standardCoffee = new StandardCoffee();
+
+        int waterForCups = water / standardCoffee.getWATER();
+        int milkForCups = milk / standardCoffee.getMILK();
+        int coffeeBeansForCups = coffeeBeans / standardCoffee.getCOFFEE_BEANS();
+
+        int minCupsQuantity = findMinCupsQuantity(waterForCups, milkForCups, coffeeBeansForCups);
+        checkHowMuchCups(cups, minCupsQuantity);
+    }
+
+    public static void checkHowMuchCups(int cups, int minCupsQuantity) {
+        if (cups == minCupsQuantity) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cups > minCupsQuantity) {
+            System.out.printf("No, I can make only %d cup(s) of coffee", minCupsQuantity);
+        } else {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)"
+                    , minCupsQuantity - cups);
+        }
+    }
+
+    public static int findMinCupsQuantity(int waterForCups, int milkForCups, int coffeeBeansForCups) {
+        int min;
+
+        if (waterForCups < milkForCups) {
+            min = Math.min(waterForCups, coffeeBeansForCups);
+        } else {
+            min = Math.min(milkForCups, coffeeBeansForCups);
+        }
+        return min;
     }
 }
---
+++ Coffee Machine/task/src/machine/StandardCoffee.java
@@ -0,0 +1,19 @@
+package machine;
+
+public class StandardCoffee {
+    final int WATER = 200;
+    final int MILK = 50;
+    final int COFFEE_BEANS = 15;
+
+    public int getWATER() {
+        return WATER;
+    }
+
+    public int getMILK() {
+        return MILK;
+    }
+
+    public int getCOFFEE_BEANS() {
+        return COFFEE_BEANS;
+    }
+}
