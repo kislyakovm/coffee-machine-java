6a09f7bfacba7b519605428151d83d70088b65de
kislyakovm <maksim@druble.ru>
2022-11-30 23:39:44 +0300

    stage 2/6/ Add ingredients calculator

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,8 +1,25 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        cupsCalculating();
+    }
 
+    public static void cupsCalculating() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many cups of coffee you will need: ");
+        int numOfCups = scanner.nextInt();
+
+        System.out.printf("For %d cups of coffee you will need:\n", numOfCups);
+        System.out.printf("%d ml of water\n", numOfCups * 200);
+        System.out.printf("%d ml of milk\n", numOfCups * 50);
+        System.out.printf("%d g of coffee beans\n", numOfCups * 15);
+    }
+
+    public void printActions() {
         String standardMessages = """
                 Starting to make a coffee
                 Grinding coffee beans
